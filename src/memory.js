dcc6f2f11965263616015981670ae47c6230e5b1
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-13 17:21:24 +0200

    implement shuffleCards method

--- src/memory.js
+++ src/memory.js
@@ -1,3 +1,22 @@
+const testArray = [
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' }
+];
+
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
@@ -9,7 +28,22 @@ class MemoryGame {
 
   shuffleCards() {
     // ... write your code here
+    if (this.cards) {
+      for (let i = this.cards.length - 1; i >= 1; i--) {
+        const randomNumber = Math.floor(Math.random() * i);
+        const firstElement = this.cards[i];
+        const secondElement = this.cards[randomNumber];
+        this.cards[i] = secondElement;
+        this.cards[randomNumber] = firstElement;
+      }
+      return this.cards;
+    }
+    else {
+      return undefined;
+    }
   }
+
+
 
   checkIfPair(card1, card2) {
     // ... write your code here
@@ -19,3 +53,5 @@ class MemoryGame {
     // ... write your code here
   }
 }
+
+console.log(new MemoryGame(testArray).shuffleCards());
